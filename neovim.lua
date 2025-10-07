66e3b790d6e95c10929083522ed4b8e51e063e90
gleb <robinsonkukurzo@gmail.com>
2025-10-07 15:09:51 -0700

    add files

---
+++ neovim.lua
@@ -0,0 +1,29 @@
+return {
+	{ "ellisonleao/gruvbox.nvim", opts = { 
+    palette_overrides = {
+      dark0 = "#2d2013",
+      dark1 = "#43342b",
+      dark2 = "#5a4a3f",
+      dark3 = "#716353",
+      dark4 = "#877567",
+      light0 = "#f8e8c3",
+      light1 = "#f0d9b5",
+      light2 = "#e6c8a0",
+      light3 = "#d9b28b",
+      light4 = "#cca37a",
+      red = "#e02e1f",
+      green = "#a0a81f",
+      yellow = "#e8a327",
+      blue = "#4a8e8f",
+      purple = "#c26e96",
+      aqua = "#6ea76b",
+      orange = "#e76b0f",
+    }
+  }},
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "gruvbox",
+		},
+	},
+}
